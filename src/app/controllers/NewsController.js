8f732eb2c8dcb55645eb17dc6fa7d01394749c3b
= <phamhongthuan2401@gmail.com>
2021-10-23 23:11:29 +0700

    install prettier lint-stage husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,13 @@
-class   NewsController {
+class NewsController {
     // [GET] /news
-    index       (req, res) {
+    index(req, res) {
         res.render('news');
     }
 
     // [GET] /news/:slug
-         show(req, res) {
-                   res.send('News Details!!!');
-          }
+    show(req, res) {
+        res.send('News Details!!!');
+    }
 }
 
-module.exports =             new NewsController();
+module.exports = new NewsController();
