b6c4e41bdcf7340c75b1b15f6e7f9d4eacc6f49b
= <phamhongthuan2401@gmail.com>
2021-10-23 23:02:54 +0700

    install prettier lint-stage husky

--- src/index.js
+++ src/index.js
@@ -1,51 +1,36 @@
-const express = require('express')
-const handlebars  = require('express-handlebars');
+const express = require('express');
+const handlebars = require('express-handlebars');
 const path = require('path');
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
 
 // HTTP Logger
 // app.use(morgan('combined'))
 
-app.use(express.static(path.join(__dirname, 'public\\')))
+app.use(express.static(path.join(__dirname, 'public\\')));
 
 // MiddleWare in Express
 // Submit theo code form html
-app.use(express.urlencoded(
-  {
-    extended: true
-  }
-));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 // Submit theo code js (XMLHttpRequest, fetch, axios...)
 app.use(express.json());
 
 //Template engine
-app.engine('hbs', handlebars({extname: '.hbs'}));
+app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 // console.log(path.join(__dirname, 'resources\\views'));
 
-// Routes
-app.get('/', function (req, res) {
-  res.render('home');
-});
-
-app.get('/news', function (req, res) {
-  res.render('news');
-});
-
-app.get('/search', function (req, res) {
-  // Lấy ra values query (Các từ kháo càn tìm kiếm)
-  // console.log(req.query.q);
-  res.render('search');
-});
-
-app.post('/search', function (req, res) {
-  console.log(req.body);
-  res.send('');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
